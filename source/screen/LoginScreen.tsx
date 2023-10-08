5f7e3a4eefda9ebac7442f6f86677ce3b78c04e1
“Anuwat” <anuwat.th@kkumail.com>
2023-10-08 10:01:45 +0700

    Ok

--- source/screen/LoginScreen.tsx
+++ source/screen/LoginScreen.tsx
@@ -1,24 +1,33 @@
-import { Link } from "react-router-dom";
-import Footer from "../component/Footer";
+import { Link } from 'react-router-dom';
+import Footer from '../component/Footer';
 
 import '../style/screen/LoginScreenStyle.css'
+import { useState } from 'react';
 export default function LoginScreen() {
+
+    const [warningMessage, setWarningMessage] = useState<string>('');
+
     return (
-        <div className="loginScreenContainer">
-            <div className="contentContainer">
-                <form action="" className="loginFormContainer">
-                    <div className="loginFormHeader">เข้าสู่ระบบ 🎉</div>
-                    <div className="loginFromDescription">ป้อนอีเมลและรหัสผ่านของคุณ</div>
-                    <div className="inputContainer">
+        <div className='loginScreenContainer'>
+            <button className='buttonBack material-symbols-rounded'></button>
+            <div className='contentContainer'>
+                <form action='' className='loginFormContainer'>
+                    <div className='loginFormHeader'>เข้าสู่ระบบ 🎉</div>
+                    <div className='loginFromDescription'>ป้อนอีเมลและรหัสผ่านของคุณ</div>
+                    <div className='inputContainer'>
+                        <input type='text' placeholder='อีเมล' className='inputField' />
                     </div>
-                    <div className="inputContainer">
+                    <div className='inputContainer'>
+                        <input type='password' placeholder='รหัสผ่าน' className='inputField' />
                     </div>
-                    <div className="warningMessageContainer"></div>
-                    <div className="submitButtonContainer"></div>
-                    <div className="suggestTextContainer">ถ้าคุณยังไม่มีบัญชี<Link to=''>สร้างบัญชีใหม่</Link></div>
+                    <div className='warningMessage'>{warningMessage}</div>
+                    <div className='submitButtonContainer'>
+                        <button type='submit' className='submitButton'>เข้าสู่ระบบ</button>
+                    </div>
+                    <div className='suggestTextContainer'>ถ้าคุณยังไม่มีบัญชี?&#160;&#160;<Link to='register'>สร้างบัญชีใหม่เลย</Link></div>
                 </form>
-                <Footer></Footer>
             </div>
-        </div>
+            <Footer></Footer>
+        </div >
     )
 }
