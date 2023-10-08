729997a394cf36d7e492579a1a3b7532df4c9e18
Lenzavalon <panthomblackmagic@hotmail.com>
2023-10-08 16:36:38 +0700

    database.js

--- Config/database.js
+++ Config/database.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql');
 
-const dbConfig = {
+// Create a connection pool
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'Ryuzu',
-};
+});
 
-const pool = mysql.createPool(dbConfig);
+// Handle connection errors
+pool.on('error', (err) => {
+  console.error('MySQL Pool Error:', err);
+});
 
 module.exports = pool;
