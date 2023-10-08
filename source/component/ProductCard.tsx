bfedfb8f363b7b74b83bc7f0c241aa4ea4997745
“Anuwat” <anuwat.th@kkumail.com>
2023-10-08 12:54:54 +0700

    OK

--- source/component/ProductCard.tsx
+++ source/component/ProductCard.tsx
@@ -13,15 +13,18 @@ interface productCardProps {
     star: number;
 }
 const ProductCard: React.FC<productCardProps> = ({ id, isWish, name, price, star }) => {
-    const [productName, setProductName] = useState<string>('iPhone 13 ความจุ 128GB รุ่น (PRODUCT)iPhone 13 ความจุ 128GB รุ่น');
+    const [productName, setProductName] = useState<string>('iPhone 13 ความจุ 128GB รุ่น (PRODUCT)iPhone 13 ความจุ 128GB รุ่น iPhone 13 ความจุ 128GB รุ่น (PRODUCT)iPhone 13 ความจุ 128GB รุ่น');
     const [productPrice, setProductPrice] = useState<string>('฿999,999');
     const [productStar, setProductStar] = useState<string>('5.0');
     return (
         <div className="productCardContainer">
+            <div className="wishButton">
+                <span className="material-symbols-rounded material-symbols-rounded-fill">heart</span>
+            </div>
             <img src={testImage} alt="ProductImage" className="productImage" />
             <div className="productCardName">{productName}</div>
             <div className="productCardStarContainer">
-                <span className="productCardStarSymbol material-symbols-rounded material-symbols-rounded-fill "></span>
+                <span className="productCardStarSymbol material-symbols-rounded material-symbols-rounded-fill">star_rate</span>
                 <div className="productCardStar">{productStar}</div>
             </div>
             <div className="productCardPrice">{productPrice}</div>
